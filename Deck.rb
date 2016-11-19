7e86301fcd87a8b8d2ea583eb97cd7808eefb2a3
Keindae <noblettm@mail.gvsu.edu>
2016-11-18 21:49:17 -0500

    updated comments

--- Deck.rb
+++ Deck.rb
@@ -38,7 +38,7 @@ class Deck
     @hand.push(second)
     @hand.push(third)
   end
-#Matt Noblett
+#Author: Matt Noblett
 #Purpose: This is used to help determine what style of game is being played by the user.
 #Input: checkUserInput - What style of game is to be played. It is either a 0 or 1
 #Output: N/A
@@ -53,9 +53,7 @@ class Deck
     end
   end
 
-
-
-#Matt Noblett
+#Author: Matt Noblett
 #Purpose: This function either runs each individual play and presents it to the user, or it will hide all of the extra outputs and just present the score to the user. 
 #Input: N/A
 #Output: N/A
@@ -83,12 +81,13 @@ class Deck
           endGame()
         end
   end
-#Matt Noblett
+#Author: Matt Noblett
 #Purpose: This function checks the card values against each other and removes the correct cards based on what was defined in the handout.
 #Input: N/A
 #OutPut: N/A
   def checkCards()
 		if @done == 0
+      #Pops the first four values off the stack to be evaluated by the following if statements. 
 		  first = @hand.pop
 		  second = @hand.pop
 		  third = @hand.pop
@@ -102,7 +101,8 @@ class Deck
 		    puts "Poped Value " + @cardMap[fourth][0].to_s + " of " + @cardMap[fourth][1].to_s
 		    puts "####################################################################################################"
       end
-        #Here I check against the first and the fourth card in my hand. If they match, then I discard them, and continue on.
+        #Here I check against the first and the fourth card in my hand. If they match, then I discard them
+      #the entire hand and run the checkHandSize function to fill the users hand and then re run the chcek cards.
 		  if(@cardMap[first][0] == @cardMap[fourth][0])
         if(@checkUserInput == 0)
         puts "####################################################################################################"
@@ -114,7 +114,7 @@ class Deck
         end
 				checkHandSize()
 			 	checkCards()
-		    #Here I check the suits against
+		    #Here I check the first and fourth suits against each other. This If it is true, then the middle two cards are discarded. 
 		  elsif(@cardMap[first][1] == @cardMap[fourth][1])
 		    if(@checkUserInput == 0)
         puts "####################################################################################################"
@@ -127,6 +127,7 @@ class Deck
 				checkHandSize()
 				checkCards()
 		  else
+        #If all of the conditions have failed, then just push the values back onto the stack, and get out of the function.
 				@hand.push(first)
 				@hand.push(second)
 				@hand.push(third)
@@ -190,14 +191,16 @@ class Deck
 	 @totalPoints = 0
 	  if @hand.length == 0
 		  puts "WINNER WINNER CHICKEN DINNER - Unless you are vegan, than enjoy a salad."
+      textWriter()
 	  else
 		  @hand.each do |card|
 			  @totalPoints += @cardMap[card][2]
 		  end
 		  puts "Total Score:  " + @totalPoints.to_s
+      textWriter()
 	  end
   end
-#Matt Noblett
+#Author: Matt Noblett
 #Purpose: This makes sure that there are 4 cards in the players hand before moving back into the checkCards function and continuing through the game logic. 
 #Input:
 #OutPut:
@@ -263,20 +266,29 @@ class Deck
 	  end
   end
 
-#Matt Noblett
+#Author:Matt Noblett
 #Purpose: Doesn't work. :(
 #Input: N/a
 #OutPUt:N/A
   def textWriter
-    File.open(Leaders.txt, 'w'){|file| file.write("#{@totalPoints}")}
-  end
 
+    File.open("Leaders.txt", 'w'){|file| file.write("#{@totalPoints}")}
+
+
+  end
+#Author: Matt Noblett
+#Purpose: Prints the deck to the user. 
+#Input: N/A
+#Output: N/A
   def printDeck
     @deck.each do |card|
       puts @cardMap[card][0].to_s + " of " + @cardMap[card][1].to_s
     end
   end
-
+#Author:Matt Noblett
+  #Purpose: To print the hand to the user and also was very helpful when error checking. Used to help the player know what is going on when they are playing turn by turn. 
+#Input: N/A
+#OutPut: N/A
   def printHand
     @hand.each do |card|
       puts @cardMap[card][0].to_s + " of " + @cardMap[card][1].to_s
